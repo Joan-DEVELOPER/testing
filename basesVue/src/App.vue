972ad0ecca21fb835993b40e49be9e18ea99c98f
JoanDev <contactojoancs@gmail.com>
2025-08-08 08:52:46 +0200

    classe variables AMazaing Code

--- basesVue/src/App.vue
+++ basesVue/src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
+import { ref } from 'vue'
 import HeaderComponent from './HeaderComponent.vue'
+
+let number = ref(0) // Actualitza la variable automaticament
+
+setInterval(() => {
+  number.value++
+}, 2000)
 </script>
 
 <template>
   <h1 class="title">Hola a todos</h1>
+  <h2>{{ number }}</h2>
   <HeaderComponent></HeaderComponent>
 </template>
 
